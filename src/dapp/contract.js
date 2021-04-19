194775adde0f5582d055ec8350033a04d94b9a98
Muhammad Raffiq Mohd Khalil <muhammadraffiq@gmail.com>
2021-04-20 02:28:12 +0800

    New contract

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -1,19 +1,151 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
 import Config from './config.json';
 import Web3 from 'web3';
 
 export default class Contract {
-    constructor(network, callback) {
+    constructor(network) {
 
         let config = Config[network];
-        this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
-        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-        this.initialize(callback);
+        //this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+        //this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+        //this.initialize(callback);
         this.owner = null;
+        this.flights = [];
         this.airlines = [];
         this.passengers = [];
+        this.gasLimit = 5000000;
     }
 
+
+    async initWeb3 (logCallback) {
+        if (window.ethereum) {
+            this.web3 = new Web3(window.ethereum);
+            try {
+                // Request account access
+                await window.ethereum.enable();
+            } catch (error) {
+                // User denied account access...
+                console.error("User denied account access")
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            this.web3 = new Web3(window.web3.currentProvider);
+        }
+        // If no injected web3 instance is detected, fall back to Ganache
+        else {
+            this.web3 = new Web3(new Web3.providers.WebsocketProvider('http://localhost:8545'));
+        }
+
+        const accounts = await this.web3.eth.getAccounts();
+        this.account = accounts[0];
+
+        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, this.config.appAddress, this.config.dataAddress);
+        this.flightSuretyData = new this.web3.eth.Contract(FlightSuretyData.abi, this.config.dataAddress);
+        this.flightSuretyApp.events.allEvents({fromBlock: 'latest', toBlock: 'latest'}, logCallback);
+        this.flightSuretyData.events.allEvents({fromBlock: 'latest', toBlock: 'latest'}, logCallback);
+    }
+
+    async registerFlight( flightName, flightDateTime) {
+        await this.flightSuretyApp.methods.registerFlight(flightName, flightDateTime).send({from: this.account, gas: this.gasLimit});
+    }
+
+    async submitAirline(_address) {
+        let self = this;
+        try {
+           await self.flightSuretyApp.methods.submitRegistration(_address).send({from: self.account, gas: self.gasLimit});
+        } catch (error) {
+            console.log(JSON.stringify(error));
+        }
+    }
+
+    async voteAirline(_address) {
+        let self = this;
+        try {
+            let index = await this.flightSuretyApp.methods.getRegIndex(_address).call();
+           await self.flightSuretyApp.methods.voteRegistration(index).send({from: self.account, gas: self.gasLimit});
+        } catch (error) {
+            console.log(JSON.stringify(error));
+        }
+    }
+
+    async executeAirline(_address) {
+        let self = this;
+        try {
+            let index = await this.flightSuretyApp.methods.getRegIndex(_address).call();
+           await self.flightSuretyApp.methods.executeRegistration(index).send({from: self.account, gas: self.gasLimit});
+        } catch (error) {
+            console.log(JSON.stringify(error));
+        }
+    }
+
+    async getCurrentFlights() {
+        return await this.flightSuretyApp.methods.getCurrentFlights().call();
+    }
+
+    async getFlightInformation(key) {
+        return await this.flightSuretyApp.methods.getFlightInformation(key).call();
+    }
+
+    async fundAirline() {
+        await this.flightSuretyApp.methods.payAirline().send({from: this.account, value: 10000000000000000000});
+    }
+
+    async setOperationalStatus(enabled) {
+        await this.flightSuretyData.methods.setOperatingStatus(enabled).send({from: this.account});
+    }
+
+    async setOperationalStatusFalseApp(enabled) {
+        await this.flightSuretyApp.methods.setOperatingStatus(enabled).send({from: this.account});
+    }
+
+    /*
+    async authorizeContract(contractAddress) {
+        await this.flightSuretyData.methods.authorizeCaller(contractAddress).send({from: this.account});
+    }
+
+    async deauthorizeContract(contractAddress) {
+        await this.flightSuretyData.methods.deauthorizeCaller(contractAddress).send({from: this.account});
+    }
+*/
+    async isOperational() {
+        return await this.flightSuretyApp.methods.isOperational().call();
+    }
+
+    async getNumberOfRegisteredAirlines() {
+        return await this.flightSuretyAdata.methods.checkNumberAirlines().call();
+    }
+
+    async getNumberOfFundedAirlines() {
+        return await this.flightSuretyApp.methods.getNumberOfFundedAirlines().call();
+    }
+
+    async getContractBalance() {
+        const contractBalance =  await this.flightSuretydata.methods.getBalance().call();
+        return `${this.web3.utils.fromWei(contractBalance, 'finney')} finney`;
+    }
+/*
+    async getInsuranceBalance() {
+        const insuranceBalance =  await this.flightSuretyApp.methods.getInsuranceBalance().call();
+        return `${this.web3.utils.fromWei(insuranceBalance, 'finney')} finney`;
+    }
+    */
+
+    async purchaseInsurance(flightKey) {
+        await this.flightSuretyApp.methods.purchaseInsurance(flightKey).send({from: this.account, value: 1000000000000000000});
+    }
+
+    async payoutFunds() {
+        await this.flightSuretyApp.methods.withdrawalFunds().send({from: this.account});
+    }
+
+    async fetchFlightStatus(flightKey) {
+        await this.flightSuretyApp.methods.fetchFlightStatus(flightKey).send({from: this.account})
+    }
+
+    
+    /*
     initialize(callback) {
         this.web3.eth.getAccounts((error, accts) => {
            
@@ -53,4 +185,5 @@ export default class Contract {
                 callback(error, payload);
             });
     }
+    */
 }
--- src/dapp/index.js
+++ src/dapp/index.js
@@ -2,10 +2,33 @@
 import DOM from './dom';
 import Contract from './contract';
 import './flightsurety.css';
+//import BigNum from "bignumber";
 
+const contract = new Contract('localhost');
+let blockNumbersSeen = [];
 
 (async() => {
+    await contract.initWeb3(eventHandler);
+    await getOperationalStatus();
 
+
+    DOM.elid('update-status').addEventListener('click', refreshStatus);
+    DOM.elid('set-contractApp-false').addEventListener('click', setOperationalStatusFalseApp);
+    DOM.elid('set-statusData-false').addEventListener('click', setOperationalStatusFalse);
+    DOM.elid('set-statusData-true').addEventListener('click', setOperationalStatusTrue);
+    //DOM.elid('register-contract-address').addEventListener('click', authorizeContract);
+    //DOM.elid('unregister-contract-address').addEventListener('click', deauthorizeContract);
+    DOM.elid('submit-airline').addEventListener('click', submitAirline);
+    DOM.elid('vote-airline').addEventListener('click', voteAirline);
+    DOM.elid('execute-airline').addEventListener('click', executeAirline);
+    DOM.elid('fund-airline').addEventListener('click', fundAirline);
+    DOM.elid('register-flight').addEventListener('click', registerFlight);
+    DOM.elid('purchase-flight-insurance').addEventListener('click', purchaseInsurance);
+    DOM.elid('send-to-oracles').addEventListener('click', sendToOracles);
+    DOM.elid('collect-funds').addEventListener('click', collectFunds);
+
+
+    /*
     let result = null;
 
     let contract = new Contract('localhost', () => {
@@ -27,11 +50,210 @@ import './flightsurety.css';
         })
     
     });
-    
+    */
 
 })();
 
-/*function EnableDisable(customerAddress, ether) {
+const collectFunds = async () => {
+    await contract.payoutFunds();
+}
+
+const sendToOracles = async () => {
+    const flightSelect = DOM.elid('select-flight');
+    await contract.getFlightStatus(flightSelect.value);
+    await refreshStatus();
+}
+
+const purchaseInsurance = async () => {
+    const flightSelect = DOM.elid('select-flight');
+    //const insudranceFunds = DOM.elid('purchase-insurance');
+    await contract.purchaseInsurance(flightSelect.value);
+    await refreshStatus();
+}
+
+const registerFlight = async () => {
+    const registerFlightName = DOM.elid('register-flight-name');
+    const registerFlightDate = DOM.elid('register-flight-date');
+    const registerFlightTime = DOM.elid('register-flight-time');
+    const stringDate = `${registerFlightDate.value}T${registerFlightTime.value}Z`;
+    const flightDateTime = new Date(stringDate).getTime();
+    await contract.registerFlight(registerFlightName.value, flightDateTime);
+    await refreshStatus();
+};
+
+const fundAirline = async () => {
+    await contract.fundAirline();
+    await refreshStatus();
+};
+
+const submitAirline = async () => {
+    const element = DOM.elid('submit-airline-address');
+    await contract.submitAirline(element.value);
+    await refreshStatus();
+    element.value = '';
+};
+
+const voteAirline = async () => {
+    const element = DOM.elid('voting-airline-address');
+    await contract.voteAirline(element.value);
+    await refreshStatus();
+    element.value = '';
+};
+
+const executeAirline = async () => {
+    const element = DOM.elid('executing-airline-address');
+    await contract.executeAirline(element.value);
+    await refreshStatus();
+    element.value = '';
+};
+
+/*
+const authorizeContract = async () => {
+    let contractAddress = DOM.elid('contract-address');
+    await contract.authorizeContract(contractAddress.value);
+    contractAddress.value = '';
+}
+
+const deauthorizeContract = async () => {
+    const contractAddress = DOM.elid('contract-address');
+    await contract.deauthorizeContract(contractAddress.value);
+    contractAddress.value = '';
+}
+*/
+
+const setOperationalStatus = async (enabled) => {
+    await contract.setOperationalStatus(enabled);
+    await refreshStatus();
+}
+
+const setOperationalStatusFalse = async () => {
+    await setOperationalStatus(false);
+}
+
+const setOperationalStatusTrue = async () => {
+    await setOperationalStatus(true);
+}
+
+const setOperationalStatusFalseApp = async () => {
+    await contract.setOperationalStatusApp(false);
+    await refreshStatus();
+}
+
+async function getOperationalStatus() {
+    let contractIsOperational;
+    let numberOfRegisteredAirlines;
+    //let numberOfFundedAirlines;
+    let contractBalance;
+    let insuranceBalance;
+
+    try {
+        contractIsOperational = await contract.isOperational();
+    } catch (error) {
+        contractIsOperational = "Error - Check console for details";
+        console.log(error);
+    }
+
+    try {
+        numberOfRegisteredAirlines = await contract.getNumberOfRegisteredAirlines();
+    } catch (error) {
+        numberOfRegisteredAirlines = "Error - Check console for details";
+        console.log(error);
+    }
+/*
+    try {
+        numberOfFundedAirlines = await contract.getNumberOfFundedAirlines();
+    } catch (error) {
+        numberOfFundedAirlines = "Error - Check console for details";
+        console.log(error);
+    }
+*/
+    try {
+        contractBalance = await contract.getContractBalance();
+    } catch (error) {
+        contractBalance = "Error - Check console for details";
+        console.log(error);
+    }
+
+    /*
+    try {
+        insuranceBalance = await contract.getInsuranceBalance();
+    } catch (error) {
+        insuranceBalance = "Error - Check console for details";
+        console.log(error);
+    }
+    */
+
+    await removeAllChildren('operational-status');
+
+    await display('operational-status', 'Operational Status', 'Status of contract', [
+        {label: 'Operational Status:', value: contractIsOperational},
+        {label: 'Number of registered airlines:', value: numberOfRegisteredAirlines},
+        //{label: 'Number of funded airlines:', value: numberOfFundedAirlines},
+        {label: 'Contract Balance:', value: contractBalance},
+        //{label: 'Funds held for insurance:', value: insuranceBalance},
+        {label: 'Last updated:', value: new Date()}
+    ]);
+
+    // make sure we have all the current flights
+    try {
+        await removeAllChildren('select-flight');
+        let currentFlights = await contract.getCurrentFlights();
+        const currentFlightSelect = DOM.elid('select-flight');
+        for (let index = 0; index < currentFlights.length; index++) {
+            let currentFlight = await contract.getFlightInformation(currentFlights[index]);
+            let option = document.createElement('option');
+            option.append(`${new Date(new BigNumber(currentFlight[1]).toNumber()).toISOString()} ${currentFlight[0]}`)
+            option.value = currentFlights[index];
+
+            currentFlightSelect.append(option);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const refreshStatus = async () => {
+    await getOperationalStatus();
+}
+
+const removeAllChildren = async (parent) => {
+    let displayDiv = DOM.elid(parent);
+    while (displayDiv.firstChild) {
+        displayDiv.removeChild(displayDiv.firstChild);
+    }
+}
+
+const display = async (divid, title, description, results) => {
+    let displayDiv = DOM.elid(divid);
+    let section = DOM.section();
+    section.appendChild(DOM.h2(title));
+    section.appendChild(DOM.h5(description));
+    results.map((result) => {
+        let row = section.appendChild(DOM.div({className: 'row text-white'}));
+        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
+        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, String(result.value)));
+        section.appendChild(row);
+    })
+    displayDiv.append(section);
+}
+
+function eventHandler(error, event) {
+    if (blockNumbersSeen.indexOf(event.transactionHash) > -1) {
+        blockNumbersSeen.splice(blockNumbersSeen.indexOf(event.transactionHash), 1);
+        return;
+    }
+    blockNumbersSeen.push(event.transactionHash);
+    console.log(event.address);
+
+    const log = DOM.elid('log-ul');
+    let newLi1 = document.createElement('li');
+    newLi1.append(`${event.event} - ${event.transactionHash}`);
+    log.appendChild(newLi1);
+}
+
+
+/*
+function EnableDisable(customerAddress, ether) {
     //Reference the Button.
     var btnSubmit = document.getElementById("submitCustomerDetails");
 
@@ -46,22 +268,9 @@ import './flightsurety.css';
         //Enable both the TextBox when TextBox is empty.
         btnSubmit.disabled = true;
     }
-}; */
+}; 
+*/
 
-function display(title, description, results) {
-    let displayDiv = DOM.elid("display-wrapper");
-    let section = DOM.section();
-    section.appendChild(DOM.h2(title));
-    section.appendChild(DOM.h5(description));
-    results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
-        section.appendChild(row);
-    })
-    displayDiv.append(section);
-
-}
 
 
 
